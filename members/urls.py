f13ce29b950d9fa61000d818db735b680ea5aa1b
ft-mammoo <mammuofficial007@gmail.com>
2025-12-02 12:53:35 +0530

    Edit Profile Url

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, EditProfileView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('edit_profile/', EditProfileView.as_view(), name='edit_profile'),
 ]
