c6eb1efa79d36ac677fece6af869c03eb941d957
ft-mammoo <mammuofficial007@gmail.com>
2025-12-02 14:20:49 +0530

    Change Password View

--- members/views.py
+++ members/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm, EditProfileForm
+from .forms import SignUpForm, EditProfileForm, PasswordsChangeForm
+from django.contrib.auth.views import PasswordChangeView
 
 class SignUpView(generic.CreateView):
     form_class = SignUpForm
@@ -16,3 +17,8 @@ class EditProfileView(generic.UpdateView):
 
     def get_object(self):
         return self.request.user
+    
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordsChangeForm
+    template_name = 'registration/change_password.html'
+    success_url = reverse_lazy('password_success')
