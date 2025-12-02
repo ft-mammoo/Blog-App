5c0ef1e27b35955a48eb6c225b648c04f8cdb067
ft-mammoo <mammuofficial007@gmail.com>
2025-12-02 14:22:09 +0530

    Change Password Urls

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView, EditProfileView
+from .views import SignUpView, EditProfileView, PasswordsChangeView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('edit_profile/', EditProfileView.as_view(), name='edit_profile'),
+    path('password/', PasswordsChangeView.as_view(), name='change_password'),
 ]
