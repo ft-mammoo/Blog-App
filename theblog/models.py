adf780439ce8063927d7c92510579d6bc763b4b6
ft-mammoo <mammuofficial007@gmail.com>
2025-12-07 16:59:39 +0530

    Added Profile of User to Models

--- theblog/models.py
+++ theblog/models.py
@@ -12,6 +12,14 @@ class Category(models.Model):
         return self.name
     def get_absolute_url(self):
         return reverse('home')
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
 class BlogPost(models.Model):
     title = models.CharField(max_length=255)
     header_image = models.ImageField(null=True, blank=True, upload_to="images/")
