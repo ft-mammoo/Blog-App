bbfae9f8a432f22da5ac45a83fe6d2db7dab776c
ft-mammoo <mammuofficial007@gmail.com>
2026-03-09 22:17:09 +0530

    Add Comment model to enable user comments on blog posts

--- theblog/models.py
+++ theblog/models.py
@@ -43,3 +43,12 @@ class BlogPost(models.Model):
     
     def get_absolute_url(self):
         return reverse('article-detail', args=(str(self.pk),))
+
+class Comment(models.Model):
+    blogpost = models.ForeignKey(BlogPost, related_name= 'comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
